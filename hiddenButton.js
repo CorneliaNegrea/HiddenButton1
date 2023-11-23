bbf9beeb4665dfbb301918e1dac0d4ebfd662840
Cornelia Negrea <cornelianegrea@yahoo.com>
2023-11-23 12:24:03 +0200

    Modification on hiddenButton js

--- hiddenButton.js
+++ hiddenButton.js
@@ -5,32 +5,44 @@ function generateButtons() {
         alert('Please insert a number greater than 2 and lower than 101');
         return;
     }
-    
+
     let buttonList = document.getElementById('buttonList');
 
     buttonList.innerHTML = '';
-
-    let min = 1;
-    let max = buttonsNr;
 
     function generateRandomIntegerInRange(min, max) {
         return Math.floor(Math.random() * (max - min + 1)) + min;
     }
 
+    let winnerButtonIndex = generateRandomIntegerInRange(1, buttonsNr);
+
     for (let i = 1; i <= buttonsNr; ++i) {
         let button = document.createElement('button');
         button.innerHTML = 'Button ' + i;
+
+        if (i === winnerButtonIndex) {
+            button.setAttribute('data-winner', 'true');
+        }
+
         button.addEventListener('click', displayText);
         buttonList.appendChild(button);
     }
-    function displayText() {
-        let text = document.getElementById("message");
-        text.style.display = "block";
-      }
 
-    function displayWinnerMessage() {
-        let text = document.getElementById("winnerMessage");
-        text.style.display = "block";
-      }
-    
+    function displayText(event) {
+        let isWinner = event.target.getAttribute('data-winner') === 'true';
+
+        if (isWinner) {
+            let winnerText = document.getElementById("winnerMessage");
+            winnerText.style.display = "block";
+            setTimeout(function () {
+                winnerText.style.display = "none";
+            }, 1000);
+        } else {
+            let regularText = document.getElementById("message");
+            regularText.style.display = "block";
+            setTimeout(function () {
+                regularText.style.display = "none";
+            }, 1000);
+        }
+    }
 }
